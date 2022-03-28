f968c9df2274813eb00c56f8ed49420d799016ce
Bruno <Bruno@test.com>
2022-03-28 08:18:14 -0300

    Criacao da entidade Cardapio

---
+++ src/main/java/br/com/bruno/reis/hasfood/entity/Cardapio.java
@@ -0,0 +1,29 @@
+package br.com.bruno.reis.hasfood.entity;
+
+
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "TBL_CARDAPIO")
+public class Cardapio {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_CARDAPIO")
+	private long IdCardapio;
+	
+	@Column(name = "ID_RESTAURANTE")
+	private long IdRestaurante;
+}
